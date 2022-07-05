7340115a4ca6a1245dc5484b5d42d577f12f9b5f
marezelej <marlej1997@gmail.com>
2022-07-06 00:32:18 +0200

    Use version on local storage

--- store/expenses.js
+++ store/expenses.js
@@ -1,17 +1,22 @@
 export const state = () => ({
-  list: []
+  list: [],
+  version: 1
 })
 
 export const mutations = {
   add(state, expenses) {
     state.list.push(...expenses)
-    localStorage.setItem('expenses', JSON.stringify(state.list))
+    localStorage.setItem('expenses', JSON.stringify({version: state.version, list: state.list}))
   },
   load(state) {
     if (state.list.length === 0) {
       const expenses = JSON.parse(localStorage.getItem('expenses'))
       if (expenses) {
-        state.list = expenses
+        if (expenses.version !== state.version) {
+          localStorage.removeItem('expenses')
+        } else {
+          state.list = expenses.list
+        }
       }
     }
   }
